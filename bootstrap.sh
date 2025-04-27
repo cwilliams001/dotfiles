c51d6025c9b9f4ffed70197032eddecd1a969382
cwilliams001 <williamsct1@gmail.com>
2025-04-27 11:05:12 -0400

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -62,6 +62,9 @@ if [[ "$IS_DEV_MACHINE" == true ]]; then
   mkdir -p "$HOME/.oh-my-zsh/custom/themes"
   if [[ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
     echo "✅ Powerlevel10k installed."
   else
     echo "✅ Powerlevel10k already present."
